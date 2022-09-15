fc8d5bb6eade13ae7c30545669035de02d8db215
ChyunKim <co1995@naver.com>
2022-09-15 23:27:04 +0900

    chore: 세팅

--- play-app/postcss.config.js
+++ play-app/postcss.config.js
@@ -1,13 +1,6 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  // "./src/**/*.{js,ts,jsx,tsx}"
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}"],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
   },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
 };
