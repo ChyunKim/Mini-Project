ca95f99911c67172ae41d940700c547126fee412
ChyunKim <co1995@naver.com>
2022-09-16 00:45:35 +0900

    feat: persist store 사용

--- play-app/pages/_app.tsx
+++ play-app/pages/_app.tsx
@@ -4,15 +4,19 @@ import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { Provider } from "react-redux";
 import store from "../src/redux/store";
+import { persistor } from '../src/redux/store'
+import { PersistGate } from "redux-persist/integration/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const queryClient = new QueryClient();
 
   return (
     <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
       <QueryClientProvider client={queryClient}>
         <Component {...pageProps} />
       </QueryClientProvider>
+      </PersistGate>
     </Provider>
   );
 }
